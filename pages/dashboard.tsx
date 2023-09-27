d9388421846852347c31abc2ebe1c37862b0b1dd
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-09-27 15:42:54 -0400

    main colours and layout

---
+++ pages/dashboard.tsx
@@ -0,0 +1,55 @@
+import { UserOutlined } from "@ant-design/icons";
+import { Card, Col, FloatButton, Row } from "antd";
+
+export default function Dashboard() {
+    return (
+        <Row>
+            <FloatButton.Group 
+                icon={<UserOutlined />} 
+                trigger="hover" 
+                style={{ left: 30}} 
+            >
+                <FloatButton/>
+                <FloatButton/>
+                <FloatButton/>
+            </FloatButton.Group>
+            <Col span={14}>
+                
+            </Col>
+            <Col span={10} className="p-10">
+                <Row gutter={[0, 10]}>
+                    <Col span={24}>
+                        <Card title="hello" className="shadow-md">
+                            <p>Card content</p>
+                        </Card>
+                    </Col>
+                    <Col span={24}>
+                        <Card title="hello" className="shadow-md">
+                            <p>Card content</p>
+                        </Card>
+                    </Col>
+                    <Col span={24}>
+                        <Card title="hello" className="shadow-md">
+                            <p>Card content</p>
+                        </Card>
+                    </Col>
+                    <Col span={24}>
+                        <Card title="hello" className="shadow-md">
+                            <p>Card content</p>
+                        </Card>
+                    </Col>
+                    <Col span={24}>
+                        <Card title="hello" className="shadow-md">
+                            <p>Card content</p>
+                        </Card>
+                    </Col>
+                    <Col span={24}>
+                        <Card title="hello" className="shadow-md">
+                            <p>Card content</p>
+                        </Card>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+    )
+}
