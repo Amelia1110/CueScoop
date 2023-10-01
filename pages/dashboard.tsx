048d1a8b9d2451ad15cff5f164c32a931a6bbb24
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-10-01 18:24:26 -0400

    added skins

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,10 @@
 import { LogoutOutlined, UserOutlined } from "@ant-design/icons";
 import { Card, Col, FloatButton, Row } from "antd";
+import Image from "next/image";
 
 export default function Dashboard() {
     return (
-        <Row className="h-screen">
+        <Row className="h-screen"> 
             <FloatButton.Group 
                 icon={<UserOutlined />} 
                 trigger="hover" 
@@ -11,11 +12,17 @@ export default function Dashboard() {
             >
                 <FloatButton icon={<LogoutOutlined />}/>
                 <FloatButton/>
-                <FloatButton/>
-            </FloatButton.Group>
-            <Col span={14}>
+                <FloatButton/> 
+            </FloatButton.Group> 
+            <Col span={13} className="grid place-content-center">
+                <Image 
+                    src="/choco_icecream.png" 
+                    width={500} 
+                    height={500}
+                    alt="Pink Popsicle"
+                />
             </Col>
-            <Col span={10} className="p-10 h-full overflow-auto">
+            <Col span={11} className="p-10 pl-0 h-full overflow-auto">
                 <Row gutter={[0, 10]}>
                     <Col span={24}>
                         <Card title="hello" className="shadow-md">
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
+    <main className="h-screen">
       Hello
     </main>
   )
