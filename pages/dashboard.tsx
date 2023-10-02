987188c97c2aafcc8f4acaeba2e581afa65c6fa3
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-10-01 20:31:28 -0400

    image routing

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,14 +3,6 @@ import { Card, Col, FloatButton, Row } from "antd";
 import Image from "next/image";
 
 export default function Dashboard() {
-    const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
-    var route = ""; // Remove later
-
-    // Remove later
-    if (prefix == "https://amelia1110.github.io") {
-        route = "CueScoop";
-    }
-
     return (
         <Row className="h-screen"> 
             <FloatButton.Group 
@@ -24,7 +16,7 @@ export default function Dashboard() {
             </FloatButton.Group> 
             <Col span={13} className="grid place-content-center">
                 <Image 
-                    src={`${prefix}/${route}/choco_icecream.png`}
+                    src={`/CueScoop/choco_icecream.png`}
                     width={500} 
                     height={500}
                     alt="Pink Popsicle"
