ed0a276382588fb3d7cc292ccfee1c17884ead45
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-10-03 21:19:00 -0400

    open/close flashcards'

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,30 @@
 import { LogoutOutlined, UserOutlined } from "@ant-design/icons";
-import { Card, Col, FloatButton, Row } from "antd";
+import { Button, Card, Col, FloatButton, Row } from "antd";
+import { AnimatePresence } from "framer-motion";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Dashboard() {
+    const [cardsOpen, setCardsOpen] = useState(true);
+
+
+    //TODO when changing to list:
+    /*
+    export const Notifications = ({ messages }) => (
+  <AnimatePresence>
+    {messages.map(({ id, content }) => (
+      <motion.li
+        key={id}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        {content}
+      </motion.li>
+    ))}
+  </AnimatePresence>
+)
+    */
+
     return (
         <Row className="h-screen"> 
             <FloatButton.Group 
@@ -16,13 +38,16 @@ export default function Dashboard() {
             </FloatButton.Group> 
             <Col span={13} className="grid place-content-center">
                 <Image 
-                    src={`/CueScoop/choco_icecream.png`}
+                    src={`/strawberry_icecream.png`}
                     width={500} 
                     height={500}
                     alt="Pink Popsicle"
                 />
+                <Button onClick={() => setCardsOpen(!cardsOpen)}>Start Studying (temp button)</Button>
             </Col>
             <Col span={11} className="p-10 pl-0 h-full overflow-auto">
+
+                <AnimatePresence>
                 <Row gutter={[0, 10]}>
                     <Col span={24}>
                         <Card title="hello" className="shadow-md">
@@ -55,6 +80,7 @@ export default function Dashboard() {
                         </Card>
                     </Col>
                 </Row>
+                </AnimatePresence>
             </Col>
         </Row>
     )
