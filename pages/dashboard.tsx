cc03e295470cf6a271dad3190439c7948cbf1f9d
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-10-25 16:10:40 -0400

    Designed flashcards

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { DeleteOutlined, EditOutlined, GlobalOutlined, LockFilled, LogoutOutlined, PlusOutlined, RightOutlined, UserOutlined } from "@ant-design/icons";
+import { CalendarOutlined, DeleteOutlined, EditOutlined, GlobalOutlined, LockFilled, LogoutOutlined, PlusOutlined, QuestionOutlined, RightOutlined, SettingOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Card, Col, FloatButton, Row } from "antd";
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
@@ -37,8 +37,8 @@ export default function Dashboard() {
                 style={{ left: 30}}  
             >
                 <FloatButton icon={<LogoutOutlined />}/>
-                <FloatButton/>
-                <FloatButton/> 
+                <FloatButton icon={<QuestionOutlined />}/>
+                <FloatButton icon={<SettingOutlined />}/> 
             </FloatButton.Group> 
             <motion.div 
                 initial={{ x: "100%" }} // Initial position
@@ -51,7 +51,7 @@ export default function Dashboard() {
                     height={500}
                     alt="Pink Popsicle"
                 />
-                <Button onClick={() => setCardsOpen(!cardsOpen)}>Start Studying (temp button) &gt;</Button>
+                <h1 onClick={() => setCardsOpen(!cardsOpen)} className="justify-self-center font-semibold text-2xl pt-4 text-zinc-500">Start Studying &gt;</h1>
             </motion.div>
             <AnimatePresence>
             {cardsOpen && (
@@ -64,13 +64,22 @@ export default function Dashboard() {
                 className="h-full overflow-auto w-11/24 bg-green"
             >
                 <div className="sticky top-0 z-10 bg-zinc-100 pt-10 pr-10 pb-4">
-                    <Col span={24}>
+                    <Row gutter={[8, 0]}>
+                    <Col span={21}>
                         <Card className="shadow-md h-12 flex items-center justify-center hover:bg-zinc-200 hover:border-zinc-200">
                             <div style={{ lineHeight: '1' }}>
                                 <PlusOutlined className="text-xl text-gray-600" />
                             </div>
                         </Card>
                     </Col>
+                    <Col span={3}>
+                        <Card className="shadow-md h-12 flex items-center justify-center hover:bg-zinc-200 hover:border-zinc-200">
+                            <div style={{ lineHeight: '1' }}>
+                                <CalendarOutlined className="text-xl text-gray-600" />
+                            </div>
+                        </Card>
+                    </Col>
+                    </Row>
                 </div>
                 <Row gutter={[0, 10]} className="pr-10 pb-10">
                     <Col span={24}>
--- pages/flashcard.tsx
+++ pages/flashcard.tsx
@@ -1,12 +1,65 @@
+import { LeftOutlined, RightOutlined, StarFilled, StarOutlined, SyncOutlined } from "@ant-design/icons";
 import { Card } from "antd";
 import { useState } from "react";
 
 export default function FlashCard() {
     const [cardStatus, setCardStatus] = useState("definition");
 
+    // TODO: Change this so it's based on database properties
+    const [starred, setStarred] = useState(false);
+    const [comfort, setComfort] = useState("Yellow");
+
+    const renderStar = (starred: any) => {
+        if (starred) {
+            return (
+                <div onClick={() => setStarred(!starred)}>
+                    <StarFilled className="absolute top-8 right-8 text-2xl text-gray-400"/>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div onClick={() => setStarred(!starred)}>
+                    <StarOutlined className="absolute top-8 right-8 text-2xl text-gray-400"/>
+                </div>
+            );
+        }
+    }
+
+    const renderComfort = (comfort: any) => {
+        if (comfort === "Red") {
+            return (
+                <div className="bg-red-400 border-red-500 border w-4 h-4 rounded-full" onClick={() => setComfort("Green")}/>
+            );
+        }
+        if (comfort === "Yellow") {
+            return (
+                <div className="bg-yellow-300 border-yellow-500 border w-4 h-4 rounded-full" onClick={() => setComfort("Red")}/>
+            );
+        }
+        if (comfort === "Green") {
+            return (
+                <div className="bg-lime-400 border-lime-600 border w-4 h-4 rounded-full" onClick={() => setComfort("Yellow")}/>
+            );
+        }
+    }
+
     return (
-        <main className="h-screen grid place-content-center">
-            <Card className="w-[50rem] h-[28rem] rounded-2xl grid place-content-center text-3xl font-semibold">Card Content</Card>
+        <main className="h-screen grid place-content-center gap-6">
+            <div className="flex justify-between">
+                <LeftOutlined className="text-xl text-gray-600"/>
+                <p className="text-lg font-light">1 of 24</p>
+                <RightOutlined className="text-xl text-gray-600"/>
+            </div>
+            <Card className="w-[50rem] h-[28rem] rounded-2xl grid place-content-center shadow-md">
+                {renderStar(starred)}
+                <p className=" text-3xl font-semibold ">Card Content</p>
+            </Card>
+            <div className="flex justify-between items-center mx-10">
+                <SyncOutlined className="text-xl text-gray-600"/>
+                <p className="text-lg font-light">Flashcard Set Name</p>
+                {renderComfort(comfort)}
+            </div>
         </main>
     )
 }
